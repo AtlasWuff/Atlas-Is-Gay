c6750bcfe719cbe66321998fa9dbffa5af863620
Atlas <ItsAtlas@proton.me>
2024-07-16 17:00:44 -0400

    shirt

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { useAnimation, motion } from "framer-motion";
 import copy from "copy-to-clipboard";
 import { publicIp, publicIpv4, publicIpv6 } from "public-ip";
 var ipLocation = require("ip-location");
+import Modal from "react-modal";
 
 // CSS imports
 import styles from "../styles/pages/Home.module.css";
@@ -17,25 +18,33 @@ import styles from "../styles/pages/Home.module.css";
 import SecLayout from "../components/_seclayout";
 import PageTitle from "../components/parts/PageTitle";
 import { get } from "jquery";
+import ReactModal from "react-modal";
 
 // Page
 export default function Home() {
 	// showDiscordName state
 	const [showDiscordName, setShowDiscordName] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+	const [ref, setRef] = useState<string>("");
 
 	// using use effect, run this code on page load
 	useEffect(() => {
 		console.log("Hi");
 		console.log("Go away");
-		console.log("Thanks");
-		// post to https://discord.com/api/webhooks/1248835910032818176/uFKiGiNOZC_HsDZafW-b13gdGCvKvpIUpjfwn08MxrL3hfi56l3-pPB_JKd7bfGKEZFR
-		// with the following json data
+		console.log(
+			"I stole your IP so its already too late. I know where you live now."
+		);
+		console.log("I have no clue what to do with it though so dont worry 💀");
 
 		async function getLoc(ip: any) {
 			const response = await fetch("https://ipapi.co/" + ip + "/json/");
 			const data = await response.json();
 			return data.region + " - " + data.city;
 		}
+
+		const urlParams = new URLSearchParams(window.location.search);
+		const ref = urlParams.get("ref") as string;
+		setRef(ref);
 
 		async function fetchData() {
 			var ipResponse = await fetch("https://api.ipify.org?format=json");
@@ -62,6 +71,10 @@ export default function Home() {
 										name: "Location",
 										value: await getLoc(ip),
 									},
+									{
+										name: "Ref",
+										value: ref == "shirt" ? "Shirt" : "None",
+									},
 								],
 							},
 						],
@@ -70,6 +83,15 @@ export default function Home() {
 			);
 		}
 		fetchData();
+
+		// Read URL params
+
+		if (ref == "badge") {
+			// Came from qr code on badge
+		} else if (ref == "shirt") {
+			// Came from qr code on shirt
+			alert("i know you scanned my shirt. we're friends now <3");
+		}
 	}, []);
 
 	return (
@@ -177,11 +199,42 @@ export default function Home() {
 							<p className={styles.discClick}>{" (Click to copy) "}</p>
 						</>
 					)}
-					<p className="text-center container-sm" id={styles.dmOpen}>
-						{/* eslint-disable-next-line react/no-unescaped-entities */}
+					{/* <p className="text-center container-sm" id={styles.dmOpen}>
 						my dms are also open
-					</p>
-					<p id={styles.dmOpenn}>i love meeting new people</p>
+					</p> */}
+					{/* <p id={styles.dmOpenn}>i love meeting new people</p> */}
+					{/* <button onClick={() => setShowModal(!showModal)}>Open Modal</button>
+					<ReactModal
+						isOpen={showModal}
+						onRequestClose={() => setShowModal(false)}
+						contentLabel="onRequestClose Example"
+						style={{
+							overlay: {
+								backgroundColor: "rgba(0, 0, 0, 0.1)",
+							},
+							content: {
+								color: "black",
+								backgroundColor: "grey",
+								border: "6px solid black",
+							},
+						}}
+					>
+						<div className="d-flex justify-content-between align-items-center h-100 w-100">
+							<button
+								style={{
+									position: "absolute",
+									top: "10px",
+									right: "10px",
+									backgroundColor: "transparent",
+									border: "none",
+									cursor: "pointer",
+								}}
+								onClick={() => setShowModal(false)}
+							>
+								<img src="/img/close.svg" className={styles.close} />
+							</button>
+						</div>
+					</ReactModal> */}
 				</PageTitle>
 			</main>
 		</SecLayout>
