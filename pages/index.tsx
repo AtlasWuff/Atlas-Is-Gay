63028996282a984ab0e07f7c2efd6222b518d365
Atlas <AtlasWuff@proton.me>
2025-01-14 21:00:37 -0500

    new tab tg

--- pages/index.tsx
+++ pages/index.tsx
@@ -238,7 +238,9 @@ export default function Home() {
 					Name="text-center container-sm">Atlas</h1> */}
 					<p className="text-center container-sm">
 						{/* eslint-disable-next-line react/no-unescaped-entities */}
-						<a href="https://t.me/eatmyatlass">my food channel</a>
+						<a target="_blank" href="https://t.me/eatmyatlass">
+							my food channel
+						</a>
 					</p>
 					<ul className={styles.socialList}>
 						<li>
